a509cc9fd1b6ba59a9fbfc13452eb04710b3a86f
thomasasia <thomas.harriett@gmail.com>
2020-02-06 16:40:06 -0500

    fixed (not really) the random gen

--- smithery.c
+++ smithery.c
@@ -12,6 +12,20 @@ int weaponCount = 0;
 static int currentWeaponIndex;
 static float currentWeaponCost;
 static char currentWeaponName[21];
+static float variation;
+
+
+// Sets up the materials to be used
+//      Name is the material name. Ex Iron
+//      Cost is the proportion of the weapon's base cost that must be paid to start the project
+//      valueMod is the modifier to the final value of the weapon that will be applied after completion
+//      dificulty impacts how hard it is to make weapons with this material. at smithingSkill = difficulty there is no penalty, otherwise it increases the chances of a negative result.
+static struct { char name[21]; float cost; float valueMod; int difficulty;} material[6];
+static int currentMaterialIndex;
+
+
+
+static int smithingSkill;
 
 // Generates a random int between the values of lower and upper
 int genRandInt(int lower, int upper){
@@ -21,9 +35,23 @@ int genRandInt(int lower, int upper){
 
 // Generates a random float between the values of lower and upper
 float genRandFloat(float min, float max){
+
+    //deal with negatives
+    int minneg = min < 0;
+    if(minneg){
+        max -= min;
+        min = 0;
+    }
+
+
+
     float range = (max - min);
     double div = RAND_MAX / range;
-    return min + (rand() / div);
+    float result = min + (rand() / div);
+    //50% chance it is negative if min is below 0
+    if(minneg) 
+            if(genRandInt(0,1)) result *= -1;
+    return result;
 }
 
 //adds a tuple to the tuples, and fills it with values
@@ -86,13 +114,14 @@ int customer(void){
     // Randomly selects a weapon
     currentWeaponIndex = genRandInt(0,15);
     // Varies the value by up to 20% of the value of the weapon
-    float variation = genRandFloat(-0.2,0.2) * weapon[currentWeaponIndex].value;
+    variation = genRandFloat(-0.2,0.2) * weapon[currentWeaponIndex].value;
 
     //sets the globals to the new weapon
-    currentWeaponCost = weapon[currentWeaponIndex].value + variation;
+    currentWeaponCost = weapon[currentWeaponIndex].value;
     strcpy(currentWeaponName, weapon[currentWeaponIndex].name);
 
-    printf("\"I want a %s and I will pay you $%f.\"\n", currentWeaponName, currentWeaponCost);
+    printf("\"I want a %s and I will pay you based on how good the weapon is, plus %f\%.\"\n",currentWeaponName, variation);
+    printf("\t(The base price of this weapon is %f)\n", currentWeaponCost);
     int accept = ynQuestion("Do you accept? (Y/N): ");
     // if you reject him he leaves
     if(!accept){
@@ -104,17 +133,34 @@ int customer(void){
     }
     return 0;
 }
+
+void chooseMaterial(void){
+    printf("What material will you make the %s out of?\n", currentWeaponName);
+
+
+
+
+}
+
 int main(void){
+    // sets up random generator
     srand(time(0));
+    // sets up the weapons' configurations
     configWeapons();
+
     listWeaponTypes();
+
+    //initiates starting resources & skills
     double money = 100.0;
-    printf("Starting money: %lf\n",money);
+    int smithingSkill = 1;
+
+    printf("Starting money: %lf\n", money);
+    printf("Smithing skill: %i\n", smithingSkill);
     while(1){
         int customerAccepted = customer();
         //if the customer was rejected, restart the loop
         if(!customerAccepted) continue;
-
+        chooseMaterial();
 
     }
     return 0;
